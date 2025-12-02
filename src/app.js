00a1da18105d4fc026eb07f9201adc25e8f904d8
sarasanchez3456 <saritasanche404@gmail.com>
2025-12-02 18:49:27 -0500

    feat(js): implementar funcion agregarItem

--- src/app.js
+++ src/app.js
@@ -1,5 +1,20 @@
-// Una función de suma (necesaria para el test) y mostrar en consola [cite: 47]
+// La función suma es necesaria para el test mínimo.
 function suma(a, b) {
   return a + b;
 }
+
+// La función para añadir elementos a la UI
+function agregarItem(texto) {
+  const lista = document.getElementById('lista');
+  const li = document.createElement('li');
+  li.textContent = texto;
+  lista.appendChild(li);
+}
+
+// Para que el test de la suma funcione en Node.js, debes exportar la función.
+module.exports = {
+  suma,
+  agregarItem 
+};
+
 console.log("App lista");
